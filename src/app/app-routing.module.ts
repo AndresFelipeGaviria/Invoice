106336cccc38309ad0fb12d236f03705b83e875d
andres gaviria <andresgavirialopez@hotmail.com>
2020-11-14 10:43:00 -0500

    creating tables

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProductComponent} from './tables/product/product.component';
+import {HomeComponent} from './tables/home/home.component';
+import {ClientComponent} from './tables/client/client.component';
+import {AppComponent} from './app.component'
+
+const routes: Routes = [
+{path: 'Product', component: ProductComponent},
+{path: 'Client', component: ClientComponent},
+{path: 'Home', component: HomeComponent},
+
+{path: '**', pathMatch: 'full', redirectTo: 'Home'},
+];
+
+export const app_routing = RouterModule.forRoot(routes);
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  someMethod() {
+    this.trigger.openMenu();
+  }
+}
